b2fea973f3146346d0e17c10b95bad7deb70d5b6
Amitai Burstein <amitai@gizra.com>
2015-05-15 20:52:43 -0400

    diff image

--- client/app/scripts/directives/beforeafterimageslider.js
+++ client/app/scripts/directives/beforeafterimageslider.js
@@ -13,18 +13,13 @@ angular.module('clientApp')
         first: '@',
         second: '@',
         width: '@',
-        height: '@'
+        height: '@',
+        baseline: '&',
+        regression: '&'
       },
-      template:
-        '<div class="before-after-slider">' +
-            '<div class="first-wrapper">' +
-              '<img ng-src="{{ first }}" width="{{ width }}" height="{{ height }}" alt="first" />' +
-            '</div>' +
-          '<div class="second-wrapper">' +
-            '<img ng-src="{{ second }}" width="{{ width }}" height="{{ height }}"  alt="second" />' +
-          '</div>' +
-        '</div>',
+      templateUrl: 'scripts/directives/beforeafterimageslider.html',
       link: function postLink(scope, element, attrs) {
+        console.log(scope.baseline);
         var $second_wrapper = element.find('.second-wrapper'),
           img_width = element.find('.second-wrapper img').width(),
           init_split = Math.round(img_width/2);
